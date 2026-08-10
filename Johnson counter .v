6e6fa99c95c7dba03fbef698721cbcd18311f555
mteja30082006 <mteja30082006@gmail.com>
2026-08-10 07:52:18 +0530

    Create Johnson counter .v

---
+++ Johnson counter .v
@@ -0,0 +1,14 @@
+module johnson_counter (
+    input  wire       clk,
+    input  wire       reset,
+    output reg  [3:0] q
+);
+
+always @(posedge clk or posedge reset) begin
+    if (reset)
+        q <= 4'b0000;
+    else
+        q <= {~q[0], q[3:1]};
+end
+
+endmodule
